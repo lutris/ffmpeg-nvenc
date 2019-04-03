ae82a042c3cca15aec6c58309ba2bcfcbe2a6574
Jaap-Willem Dooge <jaap-willem.dooge@outlook.com>
2019-04-03 22:50:35 +0200

    Install nv-codec-headers or ffmpeg won't build

--- build.sh
+++ build.sh
@@ -88,6 +88,7 @@ InstallNvCodecIncludes() {
     git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
     cd nv-codec-headers
     make
+    sudo make install
     cp -a include/ffnvcodec "$inc_dir"
 }
 
